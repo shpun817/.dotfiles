871251c18f173c7fd82f38bea703d803b1ee2e7d
Sanford Pun <punsanford@gmail.com>
2022-11-12 22:38:32 +0000

    Update nvim files

--- ubuntu_20_04/.config/nvim/lua/sanford/remap.lua
+++ ubuntu_20_04/.config/nvim/lua/sanford/remap.lua
@@ -2,18 +2,26 @@ local nnoremap = require("sanford.keymap").nnoremap
 local inoremap = require("sanford.keymap").inoremap
 local vnoremap = require("sanford.keymap").vnoremap
 
-nnoremap("<leader>pv", "<cmd>Ex<CR>")
-nnoremap("<leader>w", "<cmd>w<CR>")
-nnoremap("<leader>r", "<cmd>registers<CR>")
+-- Shortcuts
+nnoremap("<leader>pv", "<cmd>Ex<CR>") -- Open netrw
+nnoremap("<C-s>", "<cmd>w<CR>") -- Save file
+nnoremap("<leader>r", "<cmd>registers<CR>") -- Show contents in registers
+nnoremap("<leader>cm", ":!") -- Execute command
+nnoremap("<leader>;", "m\"A;<Esc>`\"") -- Insert semicolon at the end of the line
+inoremap("<C-;>", "<Esc>m\"A;<Esc>`\"") -- Insert semicolon at the end of the line
 
 -- Delete and Paste into selection without yanking
 nnoremap("<leader>d", "\"_d")
 vnoremap("<leader>d", "\"_d")
 vnoremap("<leader>p", "\"_dP")
 
+-- Auto-Pairing
 inoremap("{<CR>", "{<CR>}<C-o>O")
 inoremap("{<Space>", "{<Space><Space>}<left><left>")
+inoremap("[<Space>", "[<Space><Space>]<left><left>")
+inoremap("(<Space>", "(<Space><Space>)<left><left>")
 inoremap("(;", "();<left><left>")
 
--- Remove last word (overwrite to avoid developing the habit of closing browser tabs)
-inoremap("<A-w>", "<C-w>")
+-- Overwrites
+inoremap("<A-w>", "<C-w>") -- Remove last word (overwrite to avoid developing the habit of closing browser tabs)
+inoremap("<A-[>", "<C-[>") -- Remove last word (overwrite to avoid developing the habit of closing browser tabs)
